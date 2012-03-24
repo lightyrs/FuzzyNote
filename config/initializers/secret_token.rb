a5b2ef5e467f0bbebba24ac4185ceaed26437e2b
Harris Novick <harris@harrisnovick.com>
2012-03-24 06:58:25 -0400

    Rails 3.2.2, HAML, SASS, Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FuzzyNote::Application.config.secret_token = 'f0726ea9e8377071f7812a662651442064cd2bdce1a4a0378bfb4ea265d22556c22fb3822cdbb897e078e0d7b35f60a4a8b8658587a29d15052b994ec6dfed21'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+FuzzyNote::Application.routes.draw do
+  root :to => "home#index"
+end
